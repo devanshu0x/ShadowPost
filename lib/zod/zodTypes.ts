45500fec50c5443b9d2bac8e93f230fa9984cb39
devanshu0x <devanshudotdev@gmail.com>
2025-07-08 22:08:17 +0530

    add next-auth

---
+++ lib/zod/zodTypes.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+
+
+export const credentialSchema= z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+});
+
+export type CredentialType= z.infer<typeof credentialSchema>;
